1bd365a2e1c33990e11adf9cfd1f338de166d341
FSh <fsh1.dev@gmail.com>
2017-05-25 19:51:47 +0430

    start creating control class

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,28 @@
 package Controller;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TextArea chatTextArea;
+    @FXML
+    private TextArea messageTextArea;
+    @FXML
+    private ListView usersListView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }
