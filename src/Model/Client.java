a4d968790c6e337816731396b4b4e8faa98ff7a4
FSh <fsh1.dev@gmail.com>
2017-06-01 09:50:02 +0430

    update client with the new message class requirements

--- src/Model/Client.java
+++ src/Model/Client.java
@@ -2,6 +2,9 @@ package Model;
 
 import java.io.*;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by fsh on 5/25/17.
@@ -10,47 +13,52 @@ public class Client {
     private Socket connection;
     private DataOutputStream dos;
     private DataInputStream dis;
+    private int id;
     private String clientName;
+    private DateFormat dFormat;
+    private Date date;
 
-    public Client(String host, int port, String clientName){
+    public Client(String host, int port, int id, String clientName) {
+        dFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        date = new Date();
         try {
             connection = new Socket(host, port);
 //            System.out.println("Client connected");
             dos = new DataOutputStream(connection.getOutputStream());
             dos.flush();
             dis = new DataInputStream(connection.getInputStream());
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
+        this.id = id;
         this.clientName = clientName;
         // send client name to server
         try {
             send(clientName, Type.textMessage);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public Client(String host, int port){
-        this(host,port,"GhostUser");
+    public Client(String host, int port) {
+        this(host, port, 1111, "GhostUser");
     }
 
-    public void send(Message message) throws IOException{
+    public void send(Message message) throws IOException {
         dos.writeUTF(message.toString());
         dos.flush();
 //        System.out.println("Sent");
     }
 
-    public void send(String content, Type type) throws IOException{
-        send(new Message(content, type));
+    public void send(String content, Type type) throws IOException {
+        send(new Message(content, type, id, clientName, dFormat.format(date.getTime())));
     }
 
-    public DataInputStream getInputStream() throws IOException{
+    public DataInputStream getInputStream() throws IOException {
         return new DataInputStream(dis);
     }
 
-    public void close() throws IOException{
+    public void close() throws IOException {
         dis.close();
         dos.close();
         connection.close();
@@ -60,14 +68,14 @@ public class Client {
         return clientName;
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         // todo: remove this
         // this is for test
-        Client user = new Client("localhost",1234, "Farbod");
+        Client user = new Client("localhost", 1234, 1234, "Test");
         System.out.println("Client connected");
     }
 
-    public boolean isClosed(){
+    public boolean isClosed() {
         return connection.isClosed();
     }
 
